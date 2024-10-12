0031271dccf5f48559f1e822f9c05f14753eaf7f
EngMohammedyasser <mhmdyasrmohammedyasser@gmail.com>
2024-10-12 14:01:14 +0300

    Java Application

---
+++ JavaApplication3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author kareimpo
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.print("mmm");  // TODO code application logic here
+    }
+    
+}
